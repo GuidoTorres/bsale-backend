e9c64315841eeb4c86b776b4aa9661fcc374eb13
GuidoTorres <hectortorresdurand@gmail.com>
2021-12-10 11:45:55 -0500

    buscardor terminado

--- routes/Product.js
+++ routes/Product.js
@@ -1,13 +1,44 @@
-const {Product} = require("../db");
+const { Product } = require("../db");
 const router = require("express").Router();
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
 
 //get de todos los productos
 router.get("/", async (req, res) => {
   try {
     const prod = await Product.findAll();
-    res.send(prod)
+    res.status(200).json(prod);
   } catch (error) {
-    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
+// busqueda de productos
+router.get("/search", async (req, res) => {
+  const { term } = req.query;
+  try {
+    const prod = await Product.findAll();
+    const filter = prod.filter((data, index) =>
+      data.name.toLowerCase().includes(term.toLocaleLowerCase())
+    );
+    res.status(200).json(filter);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//get por categoria
+router.get("/:id", async (req, res) => {
+  const categoryId = req.params.id;
+  try {
+    const prod = await Product.findAll({
+      where: {
+        category: categoryId,
+      },
+    });
+    res.status(200).json(prod);
+  } catch (error) {
+    res.status(500).json(error);
   }
 });
 
