9e4b161291d34fd9fd497d9b5b8ed5cdeb634600
GuidoTorres <hectortorresdurand@gmail.com>
2021-12-12 18:46:10 -0500

    correcion puerto

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ app.use("/api/v1/products", productRouter);
 app.use("/api/v1/categories", categoryRouter);
 
 // Levanta el servidor en puerto 3000
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("server funcionando en puerto 3000");
 });
